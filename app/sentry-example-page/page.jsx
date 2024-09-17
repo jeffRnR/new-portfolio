2a80502f82ec396e7e3b12759564b52e8d3d6c08
jeffRnR <jeffmunyigi@gmail.com>
2024-09-17 11:53:57 +0300

    sentry issues

--- app/sentry-example-page/page.jsx
+++ app/sentry-example-page/page.jsx
@@ -3,6 +3,12 @@
 import Head from "next/head";
 import * as Sentry from "@sentry/nextjs";
 
+Sentry.init({
+  dsn: "https://e97d5903ba570ec9617d33427574a338@o4507966541660160.ingest.de.sentry.io/4507966546706512", // replace with your DSN
+  tracesSampleRate: 1.0, // adjust as needed
+  // other configurations
+});
+
 export default function Page() {
   return (
     <div>
