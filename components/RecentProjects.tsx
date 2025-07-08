af882ba2bd2c14bc7cee14398a53de3299042bc0
jeffRnR <jeffmunyigi@gmail.com>
2025-07-08 18:26:49 +0300

    update recent ui touches

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,7 +10,7 @@ const RecentProjects = () => {
             A section of {' '}
             <span className='text-purple'>recent projects</span>
         </h1>
-        <div className='flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10'>
+        <div className='flex flex-wrap items-center justify-center p-4 gap-x-20 gap-y-2 mt-4'>
             {projects.map(({id, title, des, img, iconLists, link}) => (
                 <div 
                     key={id} 
